661732292e4a49c67ba71bbea7e45741c3165f0e
Chelsea-Julie <chelseaqjulie.gmail.com>
2024-05-17 16:25:38 +0200

    bmi

---
+++ script/code.js
@@ -0,0 +1,21 @@
+function calculateBMI() {
+    var height = parseFloat(document.querySelector('#height').value);
+    var weight = parseFloat(document.querySelector('#weight').value);
+
+    var bmi = weight / ((height / 100) * (height / 100));
+
+    var bmiCategory;
+
+    if (bmi < 18.5) {
+        bmiCategory = 'Underweight';
+    } else if (bmi >= 18.5 && bmi < 25) {
+        bmiCategory = 'Normal weight';
+    } else if (bmi >= 25 && bmi < 30) {
+        bmiCategory = 'Overweight';
+    } else {
+        bmiCategory = 'Obesity';
+    }
+
+    var result = 'Your BMI is ' + bmi.toFixed(2) + ', which means you are ' + bmiCategory + '.';
+    document.querySelector('#result').innerText = result;
+}
